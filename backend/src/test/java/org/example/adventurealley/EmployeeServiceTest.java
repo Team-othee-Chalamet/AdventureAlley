ae8096dab0e1f7c3886caa12faf86960f1a5621d
hdtrap <peterpetertange@gmail.com>
2025-10-08 18:19:31 +0200

    Test for updating a nonexisting employee

--- backend/src/test/java/org/example/adventurealley/EmployeeServiceTest.java
+++ backend/src/test/java/org/example/adventurealley/EmployeeServiceTest.java
@@ -4,6 +4,7 @@ import org.example.adventurealley.catalog.dto.EmployeeDTO;
 import org.example.adventurealley.catalog.model.Employee;
 import org.example.adventurealley.catalog.repository.EmployeeRepo;
 import org.example.adventurealley.catalog.service.EmployeeService;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
@@ -13,8 +14,7 @@ import org.mockito.junit.jupiter.MockitoExtension;
 import javax.swing.text.html.Option;
 import java.util.Optional;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.when;
 
@@ -27,22 +27,44 @@ public class EmployeeServiceTest {
     @InjectMocks
     private EmployeeService employeeService;
 
+    private Long id;
+    private Employee existingEmployee;
+    private EmployeeDTO newDataEmployeeDTO;
+
+    @BeforeEach
+    void setup () {
+        id = 1l;
+        existingEmployee = new Employee("OldName", "OldSurname", "OldStaffId", "OldPassword");
+        newDataEmployeeDTO = new EmployeeDTO("NewName", "NewSurname", "NewStaffId", "NewPassword");
+    }
+
     @Test
     void testUpdateEmployeeIsPresent() {
         //Arrange
-        Long id = 1L;
-        Employee existingEmployee = new Employee("OldName", "OldSurname", "OldStaffId", "OldPassword");
-        EmployeeDTO newDataEmployeeDTO = new EmployeeDTO("NewName", "NewSurname", "NewStaffId", "NewPassword");
         when(employeeRepo.findById(id)).thenReturn(Optional.of(existingEmployee));
-        when(employeeRepo.save(any(Employee.class)))
-                /* When you ask mocks to perform methods, it is recorded as an invocation, in this case
+        /* When you ask mocks to perform methods, it is recorded as an invocation, in this case
                 employeeRepo.save() is the invocation, invocation.getArgument(0) returns the argument passed to
                 employeeRepo.save([argument]) - in this case the Employee we are trying to save */
+        when(employeeRepo.save(any(Employee.class)))
                 .thenAnswer(invocation -> invocation.getArgument(0));
         //Act
         EmployeeDTO updatedEmployeeDTO = employeeService.updateEmployee(id, newDataEmployeeDTO);
 
         //Assert
         assertEquals(newDataEmployeeDTO, updatedEmployeeDTO);
+    }
+
+    @Test
+    void testUpdateEmployeeIsNotPresent() {
+        // Arrange
+        when(employeeRepo.findById(id)).thenReturn(Optional.empty());
+
+        //Act
+        //I expect employeeService... to throw a RuntimeException, and I store that as a variable
+        RuntimeException runtimeException = assertThrows(RuntimeException.class, () ->
+                employeeService.updateEmployee(id, newDataEmployeeDTO));
+
+        //Assert
+        assertEquals("No employee found with ID: "+id, runtimeException.getMessage());
     }
 }
