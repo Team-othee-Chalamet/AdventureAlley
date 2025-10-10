f71d5be779df84434080874a3c607939a8e7aef7
hdtrap <peterpetertange@gmail.com>
2025-10-10 13:15:00 +0200

    Login now works!

--- frontend/scripts/login.js
+++ frontend/scripts/login.js
@@ -1,9 +1,9 @@
 console.log("login.js loaded");
 
-// Wait for DOM to load
+//Wait for DOM to load
 document.addEventListener("DOMContentLoaded", initApp);
 
-// Import post function
+//Import post function
 import { post } from "../fetchUtil.js"
 
 function initApp(){
@@ -12,25 +12,27 @@ function initApp(){
 
 }
 
-// Add event listener to the form
+//Add eventlistener to the form
 function initLogin() {
     const form = document.getElementById("loginForm")
     form.addEventListener("submit", handleLogin);
 }
 
-// Handle form submission
+//Handle login
 async function handleLogin(event) {
     event.preventDefault();
     const body = {
         staffId: event.target.staffId.value,
         password: event.target.password.value
     };
+    //Remove token if one exists (navigating to login page always logs you out)
+    localStorage.removeItem("token");
     try {
         const response = await post("http://localhost:8080/api/auth/login", body);
         console.log("Login successful:", response);
-        // Store the token in localStorage
+        //Store the token in localStorage
         localStorage.setItem("token", response.token);
-        // Redirect to another page (e.g., dashboard.html)
+        //Redirect to landing page
         window.location.href = "employeeLanding.html";
     } catch (error) {
         console.error("Login failed:", error.message);
